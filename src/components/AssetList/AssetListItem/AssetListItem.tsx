8a7b540f19cbc21ec62aa47af89dd3061c291466
Oscar Hoang <oscar@hustlebox.ca>
2022-10-30 13:49:48 -0600

    style: replace styled-components with chakra-ui components

--- src/components/AssetList/AssetListItem/AssetListItem.tsx
+++ src/components/AssetList/AssetListItem/AssetListItem.tsx
@@ -1,19 +1,18 @@
+import { Tbody, Td, Tr } from '@chakra-ui/react';
 import React from 'react';
 import { AssetState } from '../../../redux/slices/assets/slice';
-import { StyledAssetListItem, StyledHeader, StyledText } from './styles';
 
 type AssetListItemProps = { asset: AssetState };
 
 export const AssetListItem = ({ asset }: AssetListItemProps) => {
     return (
-        <StyledAssetListItem>
-            {/* TODO add image of asset */}
-            <StyledHeader>
-                <StyledText color="primary">{asset.name}</StyledText>
-                <StyledText>{asset.description}</StyledText>
-                <StyledText>{asset.quantity}</StyledText>
-                <StyledText>{asset.status}</StyledText>
-            </StyledHeader>
-        </StyledAssetListItem>
+        <Tbody>
+            <Tr>
+                <Td>{asset.name}</Td>
+                <Td>{asset.description}</Td>
+                <Td isNumeric>{asset.quantity}</Td>
+                <Td>{asset.status}</Td>
+            </Tr>
+        </Tbody>
     );
 };
--- src/components/AssetList/AssetListItems/AssetListItems.tsx
+++ src/components/AssetList/AssetListItems/AssetListItems.tsx
@@ -1,16 +1,42 @@
 import React from 'react';
 import { RootState } from '../../../redux/store';
 import { AssetListItem } from '../AssetListItem/AssetListItem';
-import { StyledAssetListItems } from './styles';
 import { useSelector } from 'react-redux';
+import { Table, TableContainer, Text, Th, Thead, Tr } from '@chakra-ui/react';
 
 export const AssetListItems = () => {
     const assets = useSelector((state: RootState) => state.assets);
     return (
-        <StyledAssetListItems>
-            {assets.map((asset) => (
-                <AssetListItem key={`${asset.id}+${asset.key}`} asset={asset} />
-            ))}
-        </StyledAssetListItems>
+        <TableContainer border="1px" borderRadius={4} borderColor="#BEBFC0">
+            <Table variant="striped">
+                <Thead>
+                    <Tr>
+                        <Th>
+                            <Text fontWeight="extrabold" casing="capitalize">
+                                Name
+                            </Text>
+                        </Th>
+                        <Th>
+                            <Text fontWeight="extrabold" casing="capitalize">
+                                Description
+                            </Text>
+                        </Th>
+                        <Th isNumeric>
+                            <Text fontWeight="extrabold" casing="capitalize">
+                                Quantity
+                            </Text>
+                        </Th>
+                        <Th>
+                            <Text fontWeight="extrabold" casing="capitalize">
+                                Status
+                            </Text>
+                        </Th>
+                    </Tr>
+                </Thead>
+                {assets.map((asset) => (
+                    <AssetListItem key={asset.id} asset={asset} />
+                ))}
+            </Table>
+        </TableContainer>
     );
 };
