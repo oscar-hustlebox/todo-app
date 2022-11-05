d2215425dc99a42bcd3822ca3470f632c876e5e4
Oscar Hoang <oscar@hustlebox.ca>
2022-11-05 16:19:16 -0600

    feat: add redux-persist and persist when refreshing page

--- src/index.tsx
+++ src/index.tsx
@@ -6,13 +6,19 @@ import reportWebVitals from './reportWebVitals';
 
 import { store } from './redux/store';
 import { Provider } from 'react-redux';
+import { persistStore } from 'redux-persist';
+import { PersistGate } from 'redux-persist/integration/react';
+
+const persistor = persistStore(store);
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
 root.render(
     <React.StrictMode>
         <Provider store={store}>
-            <App />
+            <PersistGate loading={null} persistor={persistor}>
+                <App />
+            </PersistGate>
         </Provider>
         ,
     </React.StrictMode>,
--- src/redux/store.tsx
+++ src/redux/store.tsx
@@ -1,11 +1,22 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import logger from 'redux-logger';
 import todosReducer from './slices/todos/slice';
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+const reducers = combineReducers({
+    todos: todosReducer,
+});
+
+const persistConfig = {
+    key: 'root',
+    storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, reducers);
 
 export const store = configureStore({
-    reducer: {
-        todos: todosReducer,
-    },
+    reducer: persistedReducer,
     middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 });
 
